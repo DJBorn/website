4243378f6f3a2229eeeb65d66213a39f50e375bb
ilgoo jung <idjung4@gmail.com>
2017-09-26 12:11:58 -0400

    Added custom viewport checker
    - Elements will now animate when they appear on the screen

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
-jQuery(document).ready(function() {
-    beginCSSAnimation('.fade_in', 'fadeIn');
+addEventListener('scroll', function() {
+    var fadeElements = document.getElementsByClassName("fade_in");
+    for(i = 0; i < fadeElements.length; i++) {
+        if(!fadeElements.item(i).classList.contains("reveal")) {
+            var pageDimensions = {
+                left: 0,
+                top: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+            if(intersecting(fadeElements.item(i).getBoundingClientRect(), pageDimensions))
+                fadeElements.item(i).className += " reveal";
+        }
+    }
+})
+
+$(document).ready(function() {
+    var fadeElements = document.getElementsByClassName("fade_in");
+    if(!(fadeElements.item(0).getBoundingClientRect() !== undefined &&
+         fadeElements.item(0).getBoundingClientRect().left !== undefined &&
+         fadeElements.item(0).getBoundingClientRect().top !== undefined &&
+         fadeElements.item(0).getBoundingClientRect().width !== undefined &&
+         fadeElements.item(0).getBoundingClientRect().height !== undefined && 
+         window.innerWidth !== undefined &&
+         window.innerHeight !== undefined)) {
+        for(var i = 0; i < fadeElements.length; i++) {
+            fadeElements.item(i).style.opacity = 0;
+        }
+    }
 });
 
-function beginCSSAnimation(element, addClass) {
-    jQuery(element).viewportChecker({
-        classToAdd: addClass
-    })
-}
-
-function open_long_desc() {
-    beginCSSAnimation('#bio_button', 'fadeOut');
-    beginCSSAnimation('#long_bio_desc', 'reveal');
+function intersecting(box1, box2) {
+    return (box1.left < box2.left + box2.width && box1.left + box1.width > box2.left && 
+            box1.top < box2.top + box2.height && box1.top + box1.height > box2.top);
 }
