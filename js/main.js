f7a9dc610935b9156f4c2358596eb2383cf0d4e1
ilgoo jung <idjung4@gmail.com>
2017-10-17 16:34:56 -0400

    Made title animation start on page load

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ addEventListener('scroll', function() {
     timedAnimation("slide_in", "slideIn");
 });
 
+window.onload = function () {
+    for(let i = 1; i <= 4; i++) 
+        document.getElementById("title" + i).className += " " + "title" + i + "_anim";
+}
+
 function timedAnimation(trigger, animation) {
     var elements = document.getElementsByClassName(trigger);
     for(let i = 0; i < elements.length; i++) {
